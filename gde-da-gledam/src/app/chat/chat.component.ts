9627c3df317c178223ef1847d7254a61d64ce381
Nikola Jeremic <nikola87kg@gmail.com>
2018-12-23 18:28:28 +0100

    username in chat

--- gde-da-gledam/src/app/chat/chat.component.ts
+++ gde-da-gledam/src/app/chat/chat.component.ts
@@ -9,13 +9,15 @@ import { WebsocketService } from '../_services/websocket.service';
 })
 export class ChatComponent implements OnInit {
   userId: string;
+  username: string;
   isChatOn: boolean;
   chatMessage: string;
   chatHistory = [];
 
   constructor(
-    private chatService: ChatService, 
-    public websocketService: WebsocketService) {}
+    private chatService: ChatService,
+    public websocketService: WebsocketService
+  ) {}
 
   ngOnInit() {
     this.collectChatHistory();
@@ -24,6 +26,7 @@ export class ChatComponent implements OnInit {
 
   collectChatHistory() {
     this.chatService.messages.subscribe(data => {
+      console.log(data);
       this.chatHistory.push(data);
     });
   }
@@ -31,6 +34,7 @@ export class ChatComponent implements OnInit {
   fetchId() {
     this.websocketService.userId.subscribe(data => {
       this.userId = data;
+      this.getUsername();
     });
   }
 
@@ -38,9 +42,36 @@ export class ChatComponent implements OnInit {
     this.isChatOn = !this.isChatOn;
   }
 
+  onNameChange(e) {
+    e.stopPropagation();
+  }
+
   onSend(message) {
     if (message) {
-      this.chatService.sendData(message);
+      this.chatService.sendData({
+        message: message,
+        name: this.username
+      });
+    }
+  }
+
+  setUsername() {
+    if (this.username) {
+      localStorage.setItem("account_username", this.username);
+    } else {
+      this.username = "Gost " + this.userId.slice(0, 15);
+    }
+  }
+
+  getUsername() {
+    const storageName = localStorage.getItem("account_username");
+    console.log(storageName);
+    if (storageName && storageName !== '') {
+      this.username = storageName;
+    } else if (this.userId) {
+      this.username = "Gost " + this.userId.slice(0, 15);
+    } else {
+      this.username = "Gost"; 
     }
   }
 }
